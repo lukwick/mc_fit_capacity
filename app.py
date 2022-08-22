6a3f7fe2eee97fbf2ec75d69c9329e499fe1fe4e
lukwick <wick.programming@mailbox.org>
2022-08-22 05:23:13 +0200

    implemented all studios link

--- app.py
+++ app.py
@@ -24,18 +24,52 @@ app = Flask(__name__)
 
 @app.route("/studios")
 def get_all_studios():
-    # todo: implement proper fetching of all gym ids
-    return "['123']"
+
+
+    # Request gym data from link   
+    response = requests.get("https://rsg-group.api.magicline.com/connect/v1/studio?studioTags=AKTIV-391B8025C1714FB9B15BB02F2F8AC0B2")
+
+    # Transform JSON into python format
+    data = response.json()
+
+    # Create dictionary
+    gyms_dict = {}
+
+    # Select Gym ID as key and Gym Name as value
+    for gym in data:
+        # gyms_dict[gym["id"]] = gym["studioName"]
+        gyms_dict[gym["studioName"]] = {"id": gym["id"]}
+
+    # Sort Gym Name
+    gyms_dict_sorted = {}
+    for gym in sorted(gyms_dict):
+        gyms_dict_sorted[gym] = gyms_dict[gym]
+
+    # print(gyms_dict_sorted)
+
+
+    # Transform data back into JSON format
+    gyms_json = json.dumps(gyms_dict_sorted, indent=2)
+
+
+    # print(gyms_json)
+
+
+
+    # Return JSON as endpoint
+    return gyms_json
+
+
 
 
 
 ## SHOW CAPACITY FOR ALL STUDIOS
 ## --------------------
 
-@app.route("/studios/<id>/capacity")
+@app.route("/studios/<id>")
 def get_capacacity_by_id(id):
 
-    # Request data from link
+    # Request capacity data from link
     response = requests.get("https://www.mcfit.com/de/auslastung/antwort/request.json?tx_brastudioprofilesmcfitcom_brastudioprofiles[studioId]=" +str(id))
 
     # Transform JSON into python format
