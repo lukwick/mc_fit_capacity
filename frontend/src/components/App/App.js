2a457faacede08069a729c8262fa150f15d3363c
andre-brdoch <andrebrdoch@yahoo.de>
2022-10-21 07:01:35 +0200

    refactor: Add folders to src

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App theme-light">
+      <div className="stack stack-xl">
+        <header className="App-header">
+          <div className="container">
+            <h1 className="title-xl">Find your gym</h1>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+}
