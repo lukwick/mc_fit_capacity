7582e8d0ca931bf0b950572807ea8d3e5d60a9f0
andre-brdoch <andrebrdoch@yahoo.de>
2022-10-21 08:04:19 +0200

    feat: Add basic GymItem component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,3 +1,5 @@
+import { GymItem } from '../GymItem/GymItem';
+
 import './App.css';
 
 export function App() {
@@ -6,9 +8,22 @@ export function App() {
       <div className="stack stack-xl">
         <header className="App-header">
           <div className="container">
-            <h1 className="title-xl">Find your gym</h1>
+            <h1 className="title title-xl">Find your gym</h1>
           </div>
         </header>
+
+        <main>
+          <div className="container">
+            <GymItem
+              title="Supergym New York"
+              id={123}
+              address={{
+                city: 'New York City',
+                zip: '12345',
+                street: 'Main St 12',
+              }}></GymItem>
+          </div>
+        </main>
       </div>
     </div>
   );
---
+++ frontend/src/components/GymItem/GymItem.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import './GymItem.css';
+
+export function GymItem({ id, title, address }) {
+  const { city, street, zip } = address;
+  const [isLoading, setIsLoading] = useState(false);
+  const [capacity, setCapacity] = useState(undefined);
+  const [level, setLevel] = useState(undefined);
+
+  useEffect(() => {
+    if (capacity === undefined) return;
+
+    let level;
+    if (capacity <= 30) level = 'low';
+    else if (capacity >= 70) level = 'high';
+    else level = 'medium';
+
+    setLevel(level);
+  }, [capacity]);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    // TODO: fetch
+    setCapacity(20);
+  };
+
+  return (
+    <article className="GymItem">
+      <div className="GymItem-header">
+        <header className="stack stack-s">
+          <h2 className="title title-m">{title}</h2>
+          <address>{`${street}, ${zip} ${city}`}</address>
+        </header>
+
+        <div>
+          {!capacity && (
+            <button
+              className="button button-full"
+              onClick={handleClick}
+              disabled={isLoading}>
+              {!isLoading ? 'Check capacity' : 'Loading...'}
+            </button>
+          )}
+
+          {capacity && (
+            <p className="GymItem-capacity">
+              Current capacity:{' '}
+              <span className={`GymItem-level GymItem-level-${level}`}>
+                {capacity}%
+              </span>
+            </p>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+}
+
+GymItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  address: PropTypes.shape({
+    city: PropTypes.string.isRequired,
+    zip: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    street: PropTypes.string.isRequired,
+  }).isRequired,
+};
